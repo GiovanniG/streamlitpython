e80b46f17feab46ec331f0c8067196c78fad5f71
GiovanniG <giovanni.gnascimento@gmail.com>
2023-09-25 22:45:30 -0300

    Link correto da dashboard no PowerBI.

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main():
             unsafe_allow_html=True
         )
         st.warning("Os campos de 1 a 6 devem conter apenas números ou permanecer em branco. Se necessário, utilize ponto. Por exemplo: 3.25.\n\n"
-                 "Caso queira consultar a dashboard com todos os dados, [clique aqui](https://app.powerbi.com/view?r=eyJrIjoiMGJhODM6ODctMDg2My00MTU1LThmYTAtYmFhNjZjYmQ0OTkxIiwidCI6ImIxZWQ2ZjZkLWI2ZDAtNGI5MS04ZGUwLTEzYzc1ZWQ0OTBhMiJ9).\n\n"
+                 "Caso queira consultar a dashboard com todos os dados, [clique aqui](https://app.powerbi.com/view?r=eyJrIjoiMGJhODM2ODctMDg2My00MTU1LThmYTAtYmY0YTQ5OWYzMzliIiwidCI6ImIxZWQ2ZjZkLWI2ZDAtNGI5MS04ZGUwLTEzYzc1ZWQ0OTBhMiJ9).\n\n"
                  "A dashboard é atualizada a cada 3h, começando às 0h.")
 
     # Na primeira coluna (col1), adicione um seletor antes de "Parâmetro 1" com as opções
