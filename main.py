602ebb00356fb670918996adab2d58f4af5f4c17
GiovanniG <giovanni.gnascimento@gmail.com>
2023-10-08 18:08:32 -0300

    Atualização link do PowerBI.

--- main.py
+++ main.py
@@ -57,8 +57,10 @@ def main():
             unsafe_allow_html=True
         )
         st.warning("Os campos de 1 a 6 devem conter apenas números (use ponto como separador decimal) ou permanecer em branco.\n\n"
-                 "Caso queira consultar a dashboard com todos os dados, [clique aqui](https://app.powerbi.com/view?r=eyJrIjoiMGJhODM2ODctMDg2My00MTU1LThmYTAtYmY0YTQ5OWYzMzliIiwidCI6ImIxZWQ2ZjZkLWI2ZDAtNGI5MS04ZGUwLTEzYzc1ZWQ0OTBhMiJ9).\n\n"
-                 "A dashboard é atualizada a cada 3h, começando às 0h.")
+                 "Caso queira visualizar a dashboard em tela cheia, "
+                 "[clique aqui](https://app.powerbi.com/view?r=eyJrIjoiMGJhODM2ODctMDg2My00MTU1LThmYTAtYmFyZ2l0LnVzLXdlc3QtNTAuY29udGFpbmVycy51c3VybmFtZS5zYW9hcGlkLnBvc3RyZWlkYmVzc2VzLmNvbS5icmFpbGdhdGluZyJ9&pageName=ReportSection).\n\n"
+                 "A dashboard é atualizada a cada 3h, começando às 0h."
+                 )
 
     # Na primeira coluna (col1), adicione um seletor antes de "Parâmetro 1" com as opções
     with col1:
@@ -132,7 +134,7 @@ def main():
                     # Exibe a mensagem de sucesso
                     st.success("Dados gravados com sucesso.")
 
-    # Na segunda coluna (col2), adicione a tabela sem o "Parâmetro 7"
+    # Na segunda coluna (col2), adicione a tabela sem o "Parâmetro 7" e o iframe do Power BI
     with col2:
         dados_tabela = [
             ["Código", "Parâmetro 1", "Parâmetro 2", "Parâmetro 3", "Parâmetro 4", "Parâmetro 5", "Parâmetro 6"],
@@ -161,6 +163,12 @@ def main():
 
         # Legenda no canto inferior esquerdo da tabela
         st.text("FONTE: AUTOR, 2023.")
+
+        # Adicione o iframe abaixo da tabela
+        st.markdown(
+            '<iframe title="Controle Operacional BD" width="733.5" height="501.25" src="https://app.powerbi.com/reportEmbed?reportId=e7e095bc-222c-4d22-a452-dfa1ef330e80&autoAuth=true&ctid=b1ed6f6d-b6d0-4b91-8de0-13c75ed490a2" frameborder="0" allowFullScreen="true"></iframe>',
+            unsafe_allow_html=True
+        )
 
 if __name__ == "__main__":
     main()
